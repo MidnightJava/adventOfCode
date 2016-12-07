3e44d7cbfb6e271f63fa8b78f772b983aa1d2c60
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2016-12-07 14:50:16 -0500

    Day 7 part 2

--- AocPython/src/myAoc/2016/Day07.py
+++ AocPython/src/myAoc/2016/Day07.py
@@ -10,12 +10,33 @@ def abba(s):
         if s[x] == s[x+3] and s[x+1] == s[x+2]:
             return True
     return False
-    
+
+def aba(s1, s2):
+    inbrackets = False
+    lookfor = s2[1] + s2[0] + s2[1]
+    for x in range(len(s1) - 2):
+        if s1[x] == '[':
+            inbrackets = True
+        elif s1[x] == ']':
+                inbrackets = False
+        elif not inbrackets:
+            if s1[x:x+3] == lookfor:
+                return True
+    return False
+
+def bab(s):
+    babs = []
+    for x in range(len(s) - 2):
+        if s[x] != s[x+1] and s[x] == s[x+2]:
+            babs.append(s[x:x+3])
+    return babs
+   
+#Part 1 
 with open("data/day07") as f:
     count = 0
     for line in f:
         m = re.findall("\[.*\]", line.strip())
-        if m and len(m) > 0:
+        if m:
             done = False
             for g in m:
                 if not done and abba(g[1:len(g)-1]):
@@ -24,6 +45,24 @@ with open("data/day07") as f:
                         done = True
                         break
         elif abba(line.strip()):
-                count+= 1
+            count+= 1
                         
-    print count
+    print "Part 1:", count
+
+#Part 2 
+with open("data/day07") as f:
+    count = 0
+    for line in f:
+        m = re.findall("\[[^\[\]]+\]", line.strip())
+        if m:
+            done = False
+            for g in m:
+                if not done:
+                    babs = bab(g[1:len(g)-1])
+                    for s_bab in babs:
+                        if not done:
+                            if aba(line, s_bab):
+                                count+= 1
+                                done = True
+                                break
+    print "Part 2:", count
