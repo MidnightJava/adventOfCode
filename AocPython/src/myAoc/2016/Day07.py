f54a1b7b0e5ecbe23be7be8ece1abd84d7464be6
Mark Leone <mark.leone@axiosengineering.com>
2016-12-07 20:46:21 -0500

    cleanup

--- AocPython/src/myAoc/2016/Day07.py
+++ AocPython/src/myAoc/2016/Day07.py
@@ -5,30 +5,30 @@ Created on Dec 6, 2016
 '''
 
 import re
-def abba(s):
-    for x in range(len(s) - 3):
-        if s[x] == s[x+3] and s[x+1] == s[x+2]:
+def abba(line):
+    for x in range(len(line) - 3):
+        if line[x] == line[x+3] and line[x+1] == line[x+2]:
             return True
     return False
 
-def aba(s1, s2):
+def aba(line, pattern):
     inbrackets = False
-    lookfor = s2[1] + s2[0] + s2[1]
-    for x in range(len(s1) - 2):
-        if s1[x] == '[':
+    lookfor = pattern[1] + pattern[0] + pattern[1]
+    for x in range(len(line) - 2):
+        if line[x] == '[':
             inbrackets = True
-        elif s1[x] == ']':
+        elif line[x] == ']':
                 inbrackets = False
         elif not inbrackets:
-            if s1[x:x+3] == lookfor:
+            if line[x:x+3] == lookfor:
                 return True
     return False
 
-def bab(s):
+def bab(line):
     babs = []
-    for x in range(len(s) - 2):
-        if s[x] != s[x+1] and s[x] == s[x+2]:
-            babs.append(s[x:x+3])
+    for x in range(len(line) - 2):
+        if line[x] != line[x+1] and line[x] == line[x+2]:
+            babs.append(line[x:x+3])
     return babs
    
 #Part 1 
@@ -53,9 +53,9 @@ with open("data/day07") as f:
 with open("data/day07") as f:
     count = 0
     for line in f:
+        done = False
         m = re.findall("\[[^\[\]]+\]", line.strip())
         if m:
-            done = False
             for g in m:
                 if not done:
                     babs = bab(g[1:len(g)-1])
