416957658bbbfc8cf78fd95dde0a7f4e2a9ffa3c
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2016-12-13 20:05:18 -0500

    Day 11 mods

--- AocPython/src/myAoc/2016/Day11.py
+++ AocPython/src/myAoc/2016/Day11.py
@@ -7,50 +7,83 @@ import re
 from itertools import combinations
 import copy
 
+def valid(state):
+    return True
+
+def done(state):
+    for x in xrange(1, 4):
+        if len(state[str(x)]["chips"]) != 0 or len(state[str(x)]["gens"]) != 0:
+            return False
+    return True
+
 def moves(state):
     states = []
-    nextFloors = [(state.floor + 1) % 4, (state.floor - 1) % 4]
+    nextFloors = [(state["floor"] % 4) + 1, ((state["floor"]) - 2 % 4) + 1]
     items = []
-    items.extend(state.e, *state[str(state.floor)])
+    if len(state["e"]) > 0:
+        items.extend(list(state["e"]))
+    if len(state[str(state["floor"])]["chips"]) > 0:
+        items.extend(list(state[str(state["floor"])]["chips"]))
+    if len(state[str(state["floor"])]["gens"]) > 0:
+        items.extend(list(state[str(state["floor"])]["gens"]))
     for floor in nextFloors:
         for item in items:
             possState = copy.deepcopy(state)
-            possState.floor = floor
-            possState.e = item
+            possState["e"] = item
+            possState[str(state["floor"])]["chips"] = possState[str(state["floor"])]["chips"] - set(item)
+            possState[str(state["floor"])]["gens"] = possState[str(state["floor"])]["gens"] - set(item)
+            possState["floor"] = floor
             if valid(possState):
                 states.append(possState)
         for item in combinations(items, 2):
             possState = copy.deepcopy(state)
-            possState.floor = floor
-            possState.e = item
+            possState["floor"] = floor
+            possState["e"] = item
             if valid(possState):
                 states.append(possState)
     return states
 
 def move(state):
     if done(state) :
-            print "Moves:", state.count
-            exit()
+            print "Moves:", state["count"]
+            state["done"] = True
     for st in moves(state):
-        st.count+= 1
+        st["count"]+= 1
         move(st)
 
+# count = 1
+# for x in xrange(10):
+#     print "count", (count % 4) + 1
+#     count+=1
+# print
+# 
+# count = 1
+# for x in xrange(10):
+#     print "count", ((count - 2) % 4) + 1
+#     count -= 1
+# exit()
 
 with open("data/day11") as f:
-    state = {"e":{}, "floor":1,
-             "1": {"chips": [], "gens":[]},
-             "2": {"chips": [], "gens":[]},
-             "3": {"chips": [], "gens":[]},
-             "4": {"chips": [], "gens":[]},
+    origState = {"e":set(), "floor":1, "done": False,
+             "1": {"chips": set(), "gens":set()},
+             "2": {"chips": set(), "gens":set()},
+             "3": {"chips": set(), "gens":set()},
+             "4": {"chips": set(), "gens":set()},
              "count": 0}
+    
     i = 1
     for line in f:
-        gens = re.findall("(\w+) generator", line)
-        chips = re.findall("(\w+)-compatible microchip", line)
-        state[str(i)].chips = chips
-        state[str(i)].gens = gens
+        gens = re.findall("(\w+ generator)", line)
+        chips = re.findall("(\w+-compatible microchip)", line)
+        c = origState[str(i)]
+        origState[str(i)]["chips"] = set(chips)
+        origState[str(i)]["gens"] = set(gens)
         i+= 1
-        
-    move(state)
+    state = copy.deepcopy(origState)
+    
+    while True:
+        while not state["done"]:
+            move(state)
+        state = origState
         
         
