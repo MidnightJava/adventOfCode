bd97cb2dab0c4a665e517a043d640f1a16c75114
Mark Leone <mark.leone@axiosengineering.com>
2017-12-10 15:14:31 -0500

    Optimize code

--- AocPython/src/myAoc/2017/Day10.py
+++ AocPython/src/myAoc/2017/Day10.py
@@ -4,32 +4,21 @@ Created on Dec 9, 2017
 @author: Mark
 '''
 
-inp = [189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62]
-text = '189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62'
-# text = 'AoC 2017'
-suffix = [17, 31, 73, 47, 23]
-# inp = [3, 4, 1, 5]
-
 size = 256
-# size = 5
+def getInput(part):
+	inp = '189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62'
+	if part == 1:
+		return map(lambda x: int(x), inp.split(','))
+	else:
+		suffix = [17, 31, 73, 47, 23]
+		lst = []
+		for c in inp:
+			lst.append(ord(c))
+		lst.extend(suffix)
+		return lst
 
-lst = list(xrange(size))
-
-skip = 0
-idx = 0
-
-inp2 = []
-for c in text:
-	inp2.append(ord(c))
-inp2.extend(suffix)
-
-def solve(byte=False):
-	global skip
-	global idx
-	global lst
-	global inp
-	if byte:
-		inp = inp2
+def solve(lst, idx, skip, part):
+	inp = getInput(part)
 	for l in inp:
 		if l == 0:
 			idx = (idx + skip) % size
@@ -43,28 +32,29 @@ def solve(byte=False):
 			lst[(idx +i) % size] = sublist[i]
 		idx = (idx + skip + l) % size
 		skip+=1
+	return idx, skip
 
-solve()
+idx = skip = 0
+lst = list(xrange(size))
+solve(lst, idx, skip, 1)
 print "Part 1:", lst[0] * lst[1]
 
+## Part 2 ##
+idx = skip = 0
 lst = list(xrange(size))
-idx = 0
-skip = 0
 for i in xrange(64):
-	solve(True)
-pos = 0
-p2lst = []
-# print lst
-for i in xrange(16):
-	val = 0
-	for j in xrange(pos, pos + 16):
-		val^= lst[j]
-	p2lst.append(val)
-	pos+= 16
+	idx, skip = solve(lst, idx, skip, 2)
+	
+lst2 = []
+val = 0
+for i in xrange(256):
+	val^= lst[i]
+	if (i + 1) % 16 == 0:	
+		lst2.append(val)
+		val = 0
 
-print p2lst
 out =''
-for v in p2lst:
+for v in lst2:
 	hval = hex(v)[2:]
 	if len(hval) == 1:
 		hval = '0' + hval
