6e076b50c0cc513284f869a070f0cb44df2478c3
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2017-12-11 13:18:11 -0500

    Bug fix

--- AocPython/src/myAoc/2017/Day11.py
+++ AocPython/src/myAoc/2017/Day11.py
@@ -8,7 +8,7 @@ y = 0
 z = 0
 steps = []
 f = open("data/Day11")
-grids = f.read().split(',')
+grids = f.read().strip().split(',')
 for grid in grids:
 		if 'n' == grid:
 			y+= 1
@@ -25,12 +25,12 @@ for grid in grids:
 			x-= 1
 			y+= 1
 		steps.append((abs(x) + abs(y) + abs(x+y))/2)
-# 			
+#
 print "Part 1:", (abs(x) + abs(y) + abs(x+y))/2
 print "Part 2:", max(steps)
-		
+
 		#1042 too high  not 747, 748, 825
 		#try 412, 584, 824
-		
+
 		#Part 2:
 		#3095 too high 1547 too low
