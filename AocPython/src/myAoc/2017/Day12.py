b36a919a69b8b0c5d689634eb052835491d32e8f
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2017-12-12 19:45:23 -0500

    Day 12 working

--- AocPython/src/myAoc/2017/Day12.py
+++ AocPython/src/myAoc/2017/Day12.py
@@ -7,9 +7,8 @@ from _collections import defaultdict
 
 d = defaultdict(set)
 group = set()
-global rawgroups
-rawgroups = []
-groups = []
+global groups
+groups = set()
 
 def addItems(d, items, group):
 	orig = set(group)
@@ -20,22 +19,21 @@ def addItems(d, items, group):
 				group.update(v)
 			if item.strip() in v:
 				group.add(k.strip())
-	rawgroups.append(set(group))
-# 	if len(groups) == 0:
-# 		groups.add(".".join(group))
-# 	else:
-# 		found = False
-# 		for g_s in groups:
-# 			g = set(g_s.split('.'))
-# 			if g & group:
-# 				g.update(group)
-# 				groups.remove(g_s)
-# 				g_s = ".".join(g)
-# 				groups.add(g_s)
-# 				found = True
-# 				break
-# 		if not found:
-# 			groups.add(".".join(group))
+	if len(groups) == 0:
+		groups.add(".".join(group))
+	else:
+		found = False
+		for g_s in groups:
+			g = set(g_s.split('.'))
+			if g & group:
+				g.update(group)
+				groups.remove(g_s)
+				g_s = ".".join(g)
+				groups.add(g_s)
+				found = True
+				break
+		if not found:
+			groups.add(".".join(group))
 	return group - orig
 
 with open("data/Day12") as f:
@@ -53,21 +51,10 @@ while len(added) > 0:
 
 print "Part 1:", len(group)
 
-# for k,v in d.items():
-# 	group.clear()
-# 	added = addItems(d, [k], group)
-# 	while len(added) > 0:
-# 		added = addItems(d, added, group)
-
-for rg in rawgroups:
-	for p in rg:
-		found = False
-		for g in groups:
-			if p in g:
-				found = True
-				g.update(rg)
-				break
-		if not found:
-			groups.append(rg)
+for k,v in d.items():
+	group.clear()
+	added = addItems(d, [k], group)
+	while len(added) > 0:
+		added = addItems(d, added, group)
 
 print "Part 2:", len(groups)
