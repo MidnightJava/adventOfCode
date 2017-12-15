ac209ccc00ab2bc027cd6ae9993d20c043860217
Mark Leone <mark.leone@axiosengineering.com>
2017-12-15 08:25:55 -0500

    Day 15 Part 2 partial

--- AocPython/src/myAoc/2017/Day15.py
+++ AocPython/src/myAoc/2017/Day15.py
@@ -36,34 +36,36 @@ loop = 0
 # 		print "Loop", loop
 # 	loop+= 1
 # print "Part 1:", count
-
-stack_a = []
-stack_b = []
-while True:
-	
-	seed_a = int((float(seed_a) * float(factor_a)) % divisor)
-	if seed_a % 4 == 0 and len(stack_a) < 5000000:
-		bin_a = bin(seed_a)[2:].zfill(4)
-		stack_a.append(bin_a[len(bin_a) - 16: len(bin_a)])
-	
-	seed_b = int((float(seed_b) * float(factor_b)) % divisor)
-	if seed_b % 8 == 0 and len(stack_b) < 5000000:
-		bin_b = bin(seed_b)[2:].zfill(4)
-		stack_b.append(bin_b[len(bin_b) - 16: len(bin_b)])
-		
-	if len(stack_a) == 5000000 and len(stack_b) == 5000000:
-		break
-	
-	if len(stack_a) % 10000 == 0:
-		print "Stacks", len(stack_a), len(stack_b)
-
 loop = count = 0
-while len(stack_a) > 0:
-	if stack_a.pop(0) == stack_b.pop(0)[len(bin_b) - 16: len(bin_b)]:
-		count+= 1
-	if loop % 10000 == 0:
-		print "Loop2", loop
+for _ in xrange(10):
+	seed_a = 679
+	seed_a = 65
+	seed_b = 771
+	seed_b = 8921
+	stack_a = []
+	stack_b = []
+	while len(stack_a) < 500000 or len(stack_b) < 500000:
+		
+		seed_a = int((float(seed_a) * float(factor_a)) % divisor)
+		if seed_a % 4 == 0 and len(stack_a) < 500000:
+			bin_a = bin(seed_a)[2:].zfill(4)
+			stack_a.append(bin_a[len(bin_a) - 16: len(bin_a)])
+		
+		seed_b = int((float(seed_b) * float(factor_b)) % divisor)
+		if seed_b % 8 == 0 and len(stack_b) < 500000:
+			bin_b = bin(seed_b)[2:].zfill(4)
+			stack_b.append(bin_b[len(bin_b) - 16: len(bin_b)])
+		
+		if len(stack_a) % 50000 == 0:
+			print "Stacks", len(stack_a), len(stack_b)
+	
+	while len(stack_a) > 0:
+		if stack_a.pop(0) == stack_b.pop(0):
+			print len(stack_a)
+			count+= 1
+	print "Iteration", loop
 	loop+= 1
+		
 print "Part 2:", count
 	
 #not 1224
