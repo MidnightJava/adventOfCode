aba78c460ea6afd759e475c0852c91d78ba8c452
Mark Leone <mark.leone@axiosengineering.com>
2017-12-15 23:34:28 -0500

    clean up

--- AocPython/src/myAoc/2017/Day15.py
+++ AocPython/src/myAoc/2017/Day15.py
@@ -4,38 +4,30 @@ Created on Dec 14, 2017
 @author: Mark
 '''
 
-factors = [16807, 48271]
 divisor = 2147483647
-lim1 = 40000000
-lim2 = 5000000
 
-def get_seeds():
-	return [679, 771]
-
-def get_next(part2, n):
-	seed = get_seeds()[n]
-	mod = 4 if n == 0 else 8
+def get_next(seed, mod, factor, part2):
 	count = 0
 	while True:
-		seed = seed * factors[n] % divisor
+		seed = seed * factor % divisor
 		count+= 1
-		if count % lim2 == 0 and (part2 or n == 0):
-			print "{}Gen count: {} million".format("\t\t\t" if part2 and n == 1 else "", count/1000000)
+		if count % 5000000 == 0 and (part2 or mod == 4):
+			print "{}Gen count: {} million".format("\t\t\t" if part2 and mod == 8 else "", count/1000000)
 		if not part2 or seed % mod == 0:
 			yield seed & 0xffff
 
 count = 0
-gena = get_next(False, 0)
-genb = get_next(False, 1)
-for _ in xrange(lim1):
+gena = get_next(679, 4, 16807, False)
+genb = get_next(771, 8, 48271, False)
+for _ in xrange(40000000):
 	if gena.next() == genb.next():
 		count+=1
 print "Part 1:", count
 
 count = 0
-gena = get_next(True, 0)
-genb = get_next(True, 1)
-for _ in xrange(lim2):
+gena = get_next(679, 4, 16807, True)
+genb = get_next(771, 8, 48271, True)
+for _ in xrange(5000000):
 	if gena.next() == genb.next():
 		count+=1
 print "Part 2:", count
