9a8561fa6af3f90c44f1a6338c4e36a4bd519623
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2017-12-19 16:00:29 -0500

    Day 18 working

--- AocPython/src/myAoc/2017/Day18.py
+++ AocPython/src/myAoc/2017/Day18.py
@@ -7,10 +7,9 @@ from _collections import defaultdict
 
 
 d = defaultdict(int)
-code = []
+code = map(lambda x: x.strip(), open("data/Day18").readlines())
 idx = 0
 
-code = map(lambda x: x.strip(), open("data/Day18").readlines())
 while idx >= 0 and idx < len(code):
 	instr = code[idx]
 	if "snd" in instr:
@@ -61,42 +60,29 @@ while idx >= 0 and idx < len(code):
 				idx = idx + d[offset]
 			continue
 	idx+= 1
-	
 
-sent = 0
+
+sent = [0, 0]
 d = [defaultdict(int), defaultdict(int)]
 sel = 0
 waiting  = [False, False]
-d[0]['p'] = 0
 d[1]['p'] = 1
 idx = [0,0]
 q = [[], []]
 inreg = ['', '']
 while True:
+	if idx[sel] <0 or idx[sel] > len(code):
+		#Doesn't happen, so no need to set waitign state accordingly
+		print "Index out of range"
+		break
 	instr = code[idx[sel]]
-	if waiting[sel] and len(q[(sel+1)%2]) > 0:
-		reg = inreg[sel]
-		val = q[(sel + 1) % 2].pop(0)
-		d[sel][reg] = val
-		waiting[sel] = False
-		inreg[sel] = ''
-		idx[sel]++ 1
-		continue
-	elif waiting[sel]:
-		sel = (sel + 1) % 2
-		continue
-# 	if waiting[sel] and waiting[(sel + 1) % 2]:
-# 		print "Part 2:", sent
-# 		break
-	print sent
 	if "snd" in instr:
+		sent[sel]+= 1
 		reg = instr.split()[1]
 		try:
-			q[(sel + 1) % 2].append(int(reg))
+			q[sel].append(int(reg))
 		except ValueError:
-			q[(sel + 1) % 2].append(d[sel][reg])
-		if sel == 1:
-			sent+= 1
+			q[sel].append(d[sel][reg])
 	elif 'set' in instr:
 		parts = instr.split()
 		reg, val = parts[1], parts[2]
@@ -122,7 +108,7 @@ while True:
 		parts = instr.split()
 		reg, val = parts[1], parts[2]
 		try:
-			d[sel][reg]= d[sel][reg] % int(val)
+			d[sel][reg]%= int(val)
 		except ValueError:
 			d[sel][reg]= d[sel][reg] % d[sel][val]
 	elif 'rcv' in instr:
@@ -131,11 +117,14 @@ while True:
 		if len(q[(sel + 1) % 2]) > 0:
 			val = q[(sel + 1) % 2].pop(0)
 			d[sel][reg] = val
-# 			waiting[sel] = False
+			waiting[sel] = False
 		else:
+			if len(q[sel]) == 0 and waiting[(sel + 1) % 2]:
+				print "Part2:", sent[1]
+				break
 			waiting[sel] = True
 			inreg[sel] = reg
-# 			sel = (sel + 1) % 2
+			sel = (sel + 1) % 2
 			continue
 	elif 'jgz' in instr:
 		parts = instr.split()
@@ -148,15 +137,6 @@ while True:
 		except ValueError:
 			offset = int(d[sel][parts[2]])
 		if val > 0:
-			idx[sel] = idx[sel] + offset
-# 			sel = (sel + 1) % 2
+			idx[sel]+= offset
 			continue
-	if sel == 0:
-		print idx[sel], code[idx[sel]]
-	else:
-		print "\t\t", idx[sel], code[idx[sel]]
 	idx[sel]+= 1
-# 	sel = (sel + 1) % 2
-
-	
-#16001 too high
