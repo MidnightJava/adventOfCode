b6099867289b0641fdbf6c5989ba08bb08a2bc07
Mark Leone <mark.leone@axiosengineering.com>
2018-12-20 23:58:36 -0500

    Day 17 incremental

--- AocPython/src/myAoc/2018/Day17a.py
+++ AocPython/src/myAoc/2018/Day17a.py
@@ -8,14 +8,37 @@ import re
 
 def print_grid():
 	for y in xrange(max_y + 1):
-		for x in xrange(min_x, max_x+ 1):
+		print(str(y).zfill(4), end='')
+		for x in xrange(min_x-1, max_x+ 2):
 			if (x,y) in rest:
 				print('~', end='')
 			elif (x,y) in grid:
-				print('#', end='')
+				print(grid[(x,y)], end='')
 			else:
 				print('.', end='')
 		print()
+		
+def get_next_basin(x, y):
+	while (x,y) not in clay:
+		y+= 1
+	orig_x = x
+	while (x, y) in clay:
+		x-= 1
+	x+= 1
+	bl = (x, y)
+	while (x,y) in clay:
+		y-= 1
+	tl = (x, y+1)
+	x = bl[0]
+	y = bl[1]
+	while (x,y) in clay:
+		x+= 1
+	x-= 1
+	br = (x, y)
+	while (x,y) in clay:
+		y-= 1
+	tr = (x, y+1)
+	return (tl, bl, br, tr)
 
 p = r'(\w)=(\d+), (\w)=(\d+)\.\.(\d+)'
 max_y = 0
@@ -31,7 +54,7 @@ def flow(start_x, start_y):
 	global clay
 	count+= 1
 	while (x, y+1) not in rest and (x, y+1) not in clay and y+1 < max_y:
-		rest.add((x, y+1))
+		flowing.add((x, y+1))
 		y+= 1
 	if (x-1, y) not in rest and (x-1, y) not in clay and x-1 >= min_x:
 		rest.add((x-1, y))
@@ -44,7 +67,7 @@ def flow(start_x, start_y):
 		flow(x, y-1)
 
 count = 0
-with open('./data/Day17a') as f:
+with open('./data/Day17') as f:
 	global grid
 	global clay
 	global rest
@@ -71,7 +94,8 @@ with open('./data/Day17a') as f:
 				if int(m.group(2)) > max_x: max_x = int(m.group(2))
 				if int(m.group(5)) > max_y: max_y = int(m.group(5))
 				
-flow(500, 0)
+# flow(500, 0)
+print(get_next_basin(603, 0))
 print_grid()
 
 print('x from %d to %d, y from 0 to %d' % (min_x, max_x, max_y))
