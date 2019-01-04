952ea32533ace776897b086c80ffe9731e2e601b
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2019-01-04 11:36:12 -0500

    Optimizations

--- AocPython/src/myAoc/2018/Day20.py
+++ AocPython/src/myAoc/2018/Day20.py
@@ -10,16 +10,13 @@ def BFS(x, y, seen):
 		queue = deque( [(x,y,0)])
 		while len(queue)>0:
 			x,y,d = queue.popleft()
-			if (x,y) not in grid:
-					continue
-			if grid[(x,y)] == '.':
-				grid[(x,y)]='*'
-			neighbors = [ n for n in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)] if (n[0],n[1]) in grid]
+			if (x,y) not in grid: continue
+			if grid[(x,y)] == '.': grid[(x,y)]='*' #for visualization
+			neighbors = [n for n in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)] if (n[0],n[1]) in grid]
 			seen[(x,y)] = d
 			for nb in neighbors:
-				if not nb in seen:
-					if grid[(nb[0], nb[1])] == '.':
-						d = d + 1
+				if not nb in seen or seen[(nb[0], nb[1])] > d:
+					if grid[(nb[0], nb[1])] == '.': d = d + 1
 					queue.append((nb[0],nb[1],d))
 
 def get_opts(inp):
@@ -31,10 +28,6 @@ def get_opts(inp):
 		c = inp[i]
 		if lpcount == 0:
 			if c == '|':
-				if i == len(inp) -1:
-					subs.append('')
-				elif '|' in sub:
-					sub = '(' + sub + ')'
 				subs.append(sub)
 				sub = ''
 			else:
@@ -43,12 +36,10 @@ def get_opts(inp):
 				sub+= c
 		else:
 			if c == '(': lpcount+= 1
-			elif c == ')':
-				lpcount-= 1
+			elif c == ')': lpcount-= 1
 			sub+= c
 		i+= 1
-	if len(sub) > 0:
-		subs.append(sub)
+	if len(sub): subs.append(sub)
 	return subs
 
 def parse(inp, loc, tail=None):
@@ -60,25 +51,17 @@ def parse(inp, loc, tail=None):
 	sub = ''
 	while i < len(inp):
 		c = inp[i]
-		if c == '^':
-			i+= 1
-			continue
-
-		if lpcount > 0:
+		if lpcount:
 			if c == '(': lpcount+= 1
 			elif c == ')':
-				if lpcount == 1:
-					for opt in get_opts(sub):
-						if len(opt) != 0:
-							parse(opt, (x,y), inp[i+1:])
+				lpcount-= 1
+				if lpcount == 0:
+					[parse(opt, (x,y), inp[i+1:]) for opt in get_opts(sub)]
 					i = len(inp)
 					continue
-				lpcount-= 1
 			sub+= c
-			i+= 1
 		else:
 			if c == '(': lpcount+= 1
-			elif c == ')': lpcount-= 1
 			elif c == 'N':
 				grid[(x,y-1)] = '-'
 				grid[(x,y-2)] = '.'
@@ -95,24 +78,17 @@ def parse(inp, loc, tail=None):
 				grid[(x+1,y)] = '|'
 				grid[(x+2,y)] = '.'
 				x+= 2
-			elif c == '$':
-				break
-			else:
-				print('Unexpected character: %s' % c)
-			i+= 1
+		i+= 1
 
 	if tail is not None: parse(tail, (x,y))
 
 inp = open('./data/Day20').read()
 
-grid = {(0,0): 'X'}
+grid = {(0,0) : 'X'}
 
-parse(inp, (0,0))
+parse(inp[1:-2], (0,0))
 
-minx = 0
-maxx = 0
-miny = 0
-maxy = 0
+minx = maxx = miny = maxy = 0
 
 for loc in grid.iterkeys():
 	minx = min(minx, loc[0])
@@ -123,7 +99,7 @@ for loc in grid.iterkeys():
 seen = {}
 BFS(0, 0, seen)
 print("Part 1", max(seen.values()))
-print("Part 2", len([x for x in seen.values() if x >= 1000])/2 + 1)
+print("Part 2", len([seen[x] for x in seen.keys() if seen[x] >= 1000 and grid[(x[0],x[1])] == '*']))
 
 # for y in xrange(miny-1, maxy+2, 1):
 # 	for x in xrange(minx-1, maxx+2, 1):
@@ -132,5 +108,5 @@ print("Part 2", len([x for x in seen.values() if x >= 1000])/2 + 1)
 # 		else: print('#', end='')
 # 	print()
 
-# Part 1L 3469
+# Part 1: 3469
 # Part 2: 8780
