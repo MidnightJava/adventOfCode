5e62cbe5ceee477dfbb6453e8e4b2c45c2509676
Mark Leone <mleone@lgsinnovations.com>
2019-01-21 23:43:05 -0500

    Attempt at a general solution. Not working for other data

--- AocPython/src/myAoc/2018/Day23.py
+++ AocPython/src/myAoc/2018/Day23.py
@@ -6,6 +6,8 @@ Created on Jan 11, 2019
 from __future__ import print_function
 import re
 import sys
+import math
+from __builtin__ import False
 
 all_bots = {}
 max_sig = 0
@@ -52,7 +54,6 @@ def test_coord(loc):
 	global seen
 	hits = 0
 	new_seen = set()
-	seen_length = len(seen)
 
 	for bot, d in all_bots.iteritems():
 		if (abs(bot[0] - x) + abs(bot[1] - y) + abs(bot[2] - z)) <= d:
@@ -64,7 +65,6 @@ def test_coord(loc):
 	elif hits > best_coords[0]:
 		best_coords = (hits, [(x, y, z)])
 		seen = set(new_seen)
-	return len(seen) > seen_length
 
 def findVec(point1,point2):
 	finalVector = [0 for coOrd in point1]
@@ -77,19 +77,22 @@ def findVec(point1,point2):
 def next_loc(loc, bot):
 	global all_bots
 	total_d = sum([abs(bot[i] - loc[i]) for i in range(3)])
-	target_d = abs(total_d) - abs(all_bots[bot])
+	target_d = total_d - all_bots[bot] - 1
 
 	vec = findVec(loc, bot)
-	if target_d <= 0:
-		test_coord(loc)
-		ret = loc
-	else:
-		r = abs(float(total_d) / float(target_d))
+	if target_d < 0:
 		for i in range(3):
-			vec[i] = loc[i] + int(vec[i]/r) + (1 if vec[i] >= 0 else -1)
-		test_coord(tuple(vec))
-		ret = tuple(vec)
-	return ret
+			vec[i] = -vec[i]
+	
+	if target_d == 0:
+		vec = list(loc)
+	else:
+		r = abs(float(target_d) / float(total_d))
+		for i in range(3):
+			vec[i] = loc[i] + int(vec[i] * r) + (1 if vec[i] >= 0 else -1)
+	
+	test_coord(vec)
+	return vec
 
 def bot_sort(loc):
 	def f(bot):
@@ -98,30 +101,38 @@ def bot_sort(loc):
 
 def find_bots(loc):
 	global seen
-	global min_d
 	global all_bots
 	not_seen = set(all_bots.keys()) - seen
 	not_seen = sorted(not_seen, key = bot_sort(loc) )
 	for bot in not_seen: loc = next_loc(loc, bot)
 	return loc
 
-
 def search(loc):
-	loc = find_bots(loc)
-	_, coord = best_best_coord()
-	not_seen = set(all_bots.keys()) - seen
-	not_seen = sorted(not_seen, key = bot_sort(loc))
-	for bot in not_seen:
-		d = sum([abs(bot[i] - loc[i]) for i in range(3)])
-		vec = findVec(loc, bot)
-		for i in range(3):
-			vec[i] = coord[i] + int(vec[i] / float(d)) + (1 if vec[i] >= 0 else -1)
-		for j in range(2):
-			for k in range(2):
-				for l in range(2):
-					for m in [-1,1]:
-						vec[0]+= (j*m); vec[1]+= (k*m); vec[2]+= (l*m)
-						test_coord(vec)
+	find_bots(loc)
+	done = False
+	while not done:
+		_, coord = best_best_coord()
+		not_seen = set(all_bots.keys()) - seen
+		not_seen = sorted(not_seen, key = bot_sort(loc))
+		lseen = len(seen)
+		for bot in not_seen:
+			d = sum([abs(bot[i] - coord[i]) for i in range(3)])
+			s = all_bots[bot]
+			target_d = d - s - 1
+			vec = findVec(coord, bot)
+			if target_d < 0:
+				for i in range(3): vec[i] = -vec[i]
+			for i in range(3):
+				vec[i] = coord[i] + int(vec[i] * abs(target_d) / float(d)) + (1 if vec[i] >= 0 else -1)
+			test_coord(vec)
+			for j in range(2):
+				for k in range(2):
+					for l in range(2):
+						for m in [-1,1]:
+							vec[0]+= (j*m); vec[1]+= (k*m); vec[2]+= (l*m)
+							test_coord(vec)
+						
+		if lseen == len(seen): done = True
 
 	dist, coord = best_best_coord()
 	print('Part 2: %d' % dist)
