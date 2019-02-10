9077c22378aa3191aaa28babf532a324e456eb56
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2019-02-10 18:15:06 -0500

    Day 15 work

--- AocPython/src/myAoc/2018/Day15.py
+++ AocPython/src/myAoc/2018/Day15.py
@@ -1,4 +1,7 @@
+
 from __future__ import print_function
+from heapq import heappop, heappush
+from collections import deque
 import sys
 
 def print_grid():
@@ -7,7 +10,7 @@ def print_grid():
             print(grid[(x,y)], end='')
         print()
 
-def get_actors():
+def get_units():
     l = []
     for y in range(h):
         for x in range(w):
@@ -25,7 +28,7 @@ def get_open_locs(u):
                         locs.add(loc)
     return locs
 
-with open('./data/Day15a') as f:
+with open('2018/data/Day15b') as f:
     global grid, hits
     grid = {}
     hits = {}
@@ -40,8 +43,42 @@ with open('./data/Day15a') as f:
     w = x
     h = y
 
-def find_shortest_path(s, d):
-    a = 1
+def heuristic(cell, goal):
+    return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+def shortest_path_len(start, goal):
+    paths = shortest_paths(start, goal)
+    if not paths: return None
+    return min([len(x) for x in paths])
+
+def shortest_paths(start, goal):
+    paths = []
+    queue = deque([(0, [], start)])
+    visited = {}
+    while queue:
+        l, path, current = queue.popleft()
+        x,y = current
+        if current == goal:
+            np = list(path)
+            np.append(goal)
+            paths.append(np)
+            visited[goal] = len(np)
+            continue
+        if current in visited:
+            continue
+        visited[current] = len(path)
+        for neighbor in [loc for loc in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] if loc in grid and grid[loc] == '.']:
+            np = list(path)
+            np.append(neighbor)
+            if neighbor not in visited or visited[neighbor] > len(path):
+                queue.append((len(np), np, neighbor))
+
+
+    if path: paths.append(list(path))
+    if not paths:
+        return None
+    else:
+        return filter(lambda x: len(x) == min([len(p) for p in paths]), paths)
 
 def best_loc(locs):
     ymin = h
@@ -49,7 +86,8 @@ def best_loc(locs):
     best = None
     for loc in locs:
         if not best or (loc[1] <= ymin and loc[0] <= xmin):
-            xmin = loc[0], ymin = loc[1]
+            xmin = loc[0]
+            ymin = loc[1]
             best = loc
     return best
 
@@ -59,32 +97,85 @@ def move(loc):
     paths = []
     splen = sys.maxint
     targets = []
-    for target in get_open_locs(enemy):
+    open_locs = get_open_locs(enemy)
+    if not open_locs: return False
+    for target in open_locs:
         plen = shortest_path_len(loc, target)
+        if not plen: continue
         if plen == splen:
             targets.append(target)
         elif plen < splen:
             splen = plen
             targets = [target]
+    if not targets: return False
     target = best_loc(targets)
     paths = shortest_paths(loc, target)
+    if not paths: return False
     first_steps = map(lambda x: x[0], paths)
     next_loc = best_loc(first_steps)
             
     grid[loc] = '.'
     grid[next_loc] = subj
+    return True
+
+def attack(loc):
+    enemy = 'E' if grid[loc] == 'G' else 'G'
+    x,y = loc
+    targets = []
+    for cand in [loc for loc in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)] if grid[loc] == enemy]:
+        targets.append(cand)
+    if not targets: return False
+    targets2 = []
+    minhp = sys.maxint
+    for t in targets:
+        if hits[t] < minhp:
+            minhp = hits[t]
+            targets2 = [t]
+        elif hits[t] == minhp:
+            targets2.append(t)
+    target = best_loc(targets2)
+    hits[target]-= 3
+    if hits[target] <= 0:
+        grid[target] = '.'
+    return True
+
+def tick():
+    units = get_units()
+    if not 'E' in [grid[loc] for loc in units] or not 'G' in [grid[loc] for loc in units]:
+        return False
+    # print(units)
+    # print([grid[loc] for loc in units])
+    for unit in units:
+        if not attack(unit):
+            move(unit)
+            attack(unit)
+            if not 'E' in [grid[loc] for loc in get_units()] or not 'G' in [grid[loc] for loc in get_units()]:
+                return False
+        else:
+            if not 'E' in [grid[loc] for loc in get_units()] or not 'G' in [grid[loc] for loc in get_units()]:
+                return False
+    if not 'E' in [grid[loc] for loc in get_units()] or not 'G' in [grid[loc] for loc in get_units()]:
+        return False
+    return True
+
+done = False
+global rounds
+rounds = 0
+while not done:
+    print('round %d' % rounds)
+    print_grid()
+    if tick():
+        rounds+= 1
+    else:
+        done = True
+        units = get_units()
+        print(units)
+        print([grid[loc] for loc in units])
 
 print_grid()
 
-def tick():
-    actors = get_actors()
-    print(actors)
-    for actor in actors:
-        move(actor)
-
-done = False
-while not done:
-    tick()
-    done = True
+score = rounds * sum(hits.values())
+print('rounds: %d,   hit points: %d' % (rounds, sum(hits.values())))
+print('Part 1: %d' % score)
 
 
