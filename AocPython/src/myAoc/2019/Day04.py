bb4f061588c7fdf68c564614cc5845b94f26c000
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2019-12-04 12:01:47 -0500

    Day 4 Part 2 complete

--- AocPython/src/myAoc/2019/Day04.py
+++ AocPython/src/myAoc/2019/Day04.py
@@ -1,35 +1,33 @@
-count = 0
+answers = []
+for part in[0, 1]:
+    count = 0
+    for x in range(134564, 585159+1):
+        match = 0
+        xx = str(x)
+        valid = False
+        for i in range(len(xx)):
+            if i < len(xx)-1:
+                if xx[i] == xx[i+1]:
+                    if part == 1:
+                        if i < len(xx) - 2:
+                            if xx[i] != xx[i+2] and xx[i] != xx[i-1]: valid = True
+                        elif xx[i] != xx[i-1]: valid = True
+                    else:
+                        valid = True
+        if valid or xx[-1] == xx[-2] and xx[-3] != xx[-1]: match+=1
 
-for x in range(134564, 585159+1):
-    match = 0
-    xx = str(x)
-    # valid = False
-    # for i in range(len(xx)):
-    #     if i < len(xx)-1 and xx[i] == xx[i+1]:
-    #       valid = True
-    # if valid:
-    mc = 0
-    valid = False
-    for i in range(len(xx)):
-        if i < len(xx)-1:
-            if xx[i] == xx[i+1]:
-                mc+= 1
-                if mc == 2:
-                    if i == len(xx)-2 or xx[i] != xx[i+2]: valid = True
-            else:
-                mc = 0
-    if valid or xx[-1] == xx[-2]: match+=1
+        valid = True
+        for i in range(len(xx)):
+            if i < len(xx)-1:
+                if int(xx[i]) > int(xx[i+1]):
+                    valid = False
+        if valid: match+= 1
 
-    valid = True
-    for i in range(len(xx)):
-        if i < len(xx)-1:
-            if int(xx[i]) > int(xx[i+1]):
-                valid = False
-    if valid: match+= 1
+        if match == 2: count+=1
+    answers.append(count)
 
-    if match == 2: count+=1
+print('Part 1: %d' % answers[0])
+print('Part 2: %d' % answers[1])
 
-print('Part 1: %d' % count)
-
-# part 2: 844 < ans < 1634
-# not 906, 1122, 1407, 1267
+# Part 1: 1929
+# Part 2: 1306
