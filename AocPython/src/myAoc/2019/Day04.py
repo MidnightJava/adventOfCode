eddf79fb5b658b08770395c8fe0812880742e264
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2019-12-04 01:54:18 -0500

    Day 4 part 2, not working

--- AocPython/src/myAoc/2019/Day04.py
+++ AocPython/src/myAoc/2019/Day04.py
@@ -3,16 +3,23 @@ count = 0
 for x in range(134564, 585159+1):
     match = 0
     xx = str(x)
-    valid = False
-    for i in range(len(xx)):
-        if i < len(xx)-1 and xx[i] == xx[i+1]:
-          valid = True
-    if valid: match+= 1
-
-    # mc = 0
+    # valid = False
     # for i in range(len(xx)):
     #     if i < len(xx)-1 and xx[i] == xx[i+1]:
-    #         mc+= 1
+    #       valid = True
+    # if valid:
+    mc = 0
+    fail = False
+    found = False
+    for i in range(len(xx)):
+        if i < len(xx)-1:
+            if xx[i] == xx[i+1]:
+                mc+= 1
+                if mc >2: fail = True
+                elif mc == 2: found = True
+            else:
+                mc = 0
+    if (found or xx[-1] == xx[-2]): match+=1
 
     valid = True
     for i in range(len(xx)):
@@ -25,4 +32,5 @@ for x in range(134564, 585159+1):
 
 print('Part 1: %d' % count)
 
-# part 2 < 1634
+# part 2: 844 < ans < 1634
+# not 906, 1122, 1407
