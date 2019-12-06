52e1ffc42a7853c4f1bacad7a9a5b7175f5e3307
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2019-12-06 17:45:35 -0500

    Cleanup and steramline

--- AocPython/src/myAoc/2019/Day06.py
+++ AocPython/src/myAoc/2019/Day06.py
@@ -2,62 +2,47 @@ from collections import defaultdict
 
 _map = {}
 seen = set()
-count = 0
 with open('2019/data/day06') as f:
     for line in f:
         kv = line.split(')')
         _map[kv[1].strip()] = kv[0]
-    # print(_map)
-
-leaves = set(_map.keys()) - set(_map.values())
-# print(leaves)
 
 # Part 1
-def solve(node):
-    global count
+def solve(node, res):
     while node:
         if _map.get(node, None):
             node = _map[node]
-            count+= 1
+            res['count']+= 1
             if not node in seen:
-                solve(node)
+                solve(node, res)
             seen.add(node)
         else:
             node = None
 
+leaves = set(_map.keys()) - set(_map.values())
+res = {'count': 0}
 for node in leaves:
-    solve(node)
-print('Part 1: %d' % count)
-
-paths = defaultdict(list)
-def mk_path(node):
-    global paths
-    start = node
-    while node:
-        if _map.get(node, None):
-            node = _map[node]
-            paths[start].append(node)
-        else:
-            node = None
-
-def get_dist(node, dest):
-    count = 0
-    while node:
-        if _map.get(node, None):
-            node = _map[node]
-            count+= 1
-            if node == dest:
-                node = None
-        else:
-            node = None
-    return count
+    solve(node, res)
+print('Part 1: %d' % res['count'])
 
 #Part 2
-mk_path('YOU')
-mk_path('SAN')
+def mk_path(node):
+    path = []
+    while node:
+        if _map.get(node, None):
+            node = _map[node]
+            path.append(node)
+        else:
+            return path
 
-for n in paths['YOU']:
+you_path = mk_path('YOU')
+san_path = mk_path('SAN')
+
+for n in you_path:
     n = _map.get(n)
-    if n in paths['SAN']:
-        print('Part 2: %d' % (get_dist(_map.get('YOU'), n) + get_dist(_map.get('SAN'), n)))
+    if n in san_path:
+        print('Part 2: %d' % (you_path.index(n) + san_path.index(n)))
         break
+
+# Part 1: 402879
+# Part 2: 484
