6adbf1236c91bb77ebcb459a11d4aa88d9c6bd79
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2019-12-09 19:02:57 -0500

    Tweak

--- AocPython/src/myAoc/2019/Day09.py
+++ AocPython/src/myAoc/2019/Day09.py
@@ -1,111 +1,105 @@
 with open('./2019/data/day09a') as f:
-    inp = f.readline().split(',')
+    inp = list(map(int, f.readline().split(',')))
+    [inp.append(0) for rin in range(10000)]
     input = 1
     pos = 0
     relb = 0
 
-    def get_inp(pos):
-        if pos >= len(inp):
-            for i in range(len(inp), pos+1):
-                inp.append('0')
-        return inp[pos]
-
-    def set_inp(pos, val):
-        if pos >= len(inp):
-            for i in range(len(inp), pos+1):
-                inp.append('0')
-        inp[pos] = val
-
     def get_params(m1, m2, m3):
         if m1 == 0:
-            p1 = get_inp(int(get_inp(pos+1)))
+            p1 = inp[inp[pos+1]]
         elif m1 == 1:
-            p1 = get_inp(pos+1)
+            p1 = inp[pos+1]
         elif m1 == 2:
-            p1 = str(int(get_inp(int(get_inp(pos+1)))) + relb)
+            p1 = inp[int(inp[pos+1])] + relb
         if m2 == 0:
-            p2 = get_inp(int(get_inp(pos+2)))
+            p2 = inp[inp[pos+2]]
         elif m2 == 1:
-            p2 = get_inp(pos+2)
+            p2 = inp[pos+2]
         elif m2 == 2:
-            p2 = str(int(get_inp(int(get_inp(pos+2)))) + relb)
+            p2 = inp[inp[pos+2]] + relb
         if m3 == 0:
-            p3 = int(get_inp(int(get_inp(pos+3))))
+            p3 = inp[inp[pos+3]]
         elif m3 == 1:
-            p3 = int(get_inp(pos+3))
+            p3 = inp[pos+3]
         elif m3 == 2:
-            p3 = int(get_inp(int(get_inp(pos+3)))) + relb
+            p3 = inp[inp[pos+3]] + relb
         return (p1, p2, p3)
 
-    while get_inp(pos) != 99:
-        instr = get_inp(pos)
+    while inp[pos] != 99:
+        instr = str(inp[pos])
         op = int(instr[-2]) * 10 + int(instr[-1]) if len(instr) > 1 else int(instr[0])
         m1 = int(instr[-3]) if len(instr) > 2 else 0
         m2 = int(instr[-4]) if len(instr) > 3 else 0
         m3 = int(instr[-5]) if len(instr) > 4 else 0
         if op == 1:
-            p1, p2, p3 = get_params(m1, m2, m3)
-            set_inp(p3, str(int(p1) + int(p2)))
+            # if mode = 1: p1 = pos+1 if mode = 0: p1 = inp[pos+1] if mode = 2: p1 = inp[pos+1] + relb
+            # Then when processing opcode us inp[p1]
+            p1 = inp[pos+1] if m1 == 1 else inp[inp[pos+1]]
+            p2 = inp[pos+2] if m2 == 1 else inp[inp[pos+2]]
+            p3 = inp[pos+3] if m3 == 1 else inp[inp[pos+3]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            if m2 == 2: p2 = inp[inp[pos+2] + relb]
+            if m3 == 2: p3 = inp[inp[pos+3] + relb]
+            inp[p3] = p1 + p2
             pos+= 4
         elif op == 2:
-            p1, p2, p3 = get_params(m1, m2, m3)
-            set_inp(p3, str(int(p1) * int(p2)))
+            p1 = inp[pos+1] if m1 == 1 else inp[inp[pos+1]]
+            p2 = inp[pos+2] if m2 == 1 else inp[inp[pos+2]]
+            p3 = inp[pos+3] if m3 == 1 else inp[inp[pos+3]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            if m2 == 2: p2 = inp[inp[pos+2] + relb]
+            if m3 == 2: p3 = inp[inp[pos+3] + relb]
+            inp[p3] = p1 * p2
             pos+= 4
         elif op == 3:
-            p1, p2, p3 = get_params(m1, m2, m3)
-            # if m1 == 0:
-            #     set_inp(int(get_inp(int(get_inp(pos+1)))), str(input))
-            # elif m1 == 1:
-            #     set_inp(int(get_inp(pos+1)), str(input))
-            # elif m1 == 2:
-            #     set_inp(int(get_inp(int(get_inp(pos+1))+relb)), str(input))
-            set_inp(int(p1), str(input))
+            p1 = inp[pos+1] if m1 == 1 else inp[inp[pos+1]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            inp[p1] = input
             pos+= 2
         elif op == 4: #OUTP
-            p1, p2, p3 = get_params(m1, m2, m3)
+            p1 = inp[pos+1] if m1 == 1 else inp[inp[pos+1]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
             output = p1
-            # if m1 == 0:
-            #     output = get_inp(int(p1))
-            # elif m1 == 1:
-            #     output = p1
-            # elif m1 == 2:
-            #     output = get_inp(int(p1) + relb)
-            # if m1 == 0:
-            #    out_addr = get_inp(int(get_inp(pos+1)))
-            # elif m1 == 1:
-            #     out_addr = get_inp(pos+1)
-            # elif m1 == 2:
-            #     out_addr = get_inp(int(get_inp(pos+1))+ relb)
-            # output = get_inp(int(p1))
-            # output = get_inp(int(out_addr))
-            print('Output: %s, m1: %d' % (output, m1))
+            print('Output: %d, m1: %d' % (output, m1))
             pos+= 2
         elif op == 5:#JMP IF TRUE
-            p1, p2, p3 = get_params(m1, m2, m3)
-            pos = int(p2) if int(p1) != 0 else pos+3
+            p1 = inp[pos+1] if m1 == 1 else inp[inp[pos+1]]
+            p2 = inp[pos+2] if m2 == 1 else inp[inp[pos+2]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            if m2 == 2: p2 = inp[inp[pos+2] + relb]
+            pos = p2 if p1 else pos+3
         elif op == 6:#JMP IF FALSE
-            p1, p2, p3 = get_params(m1, m2, m3)
-            pos = int(p2) if int(p1) == 0 else pos+3
+            p1 = inp[pos+1] if m1 == 1 else inp[int(inp[pos+1])]
+            p2 = inp[pos+2] if m2 == 1 else inp[int(inp[pos+2])]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            if m2 == 2: p2 = inp[inp[pos+2] + relb]
+            pos = p2 if not p1 else pos+3
         elif op == 7:#LT
-            p1, p2, p3 = get_params(m1, m2, m3)
-            if int(p1) < int(p2):
-                set_inp(p3, "1")
+            p1 = inp[pos+1] if m1 == 1 else inp[int(inp[pos+1])]
+            p2 = inp[pos+2] if m2 == 1 else inp[int(inp[pos+2])]
+            p3 = inp[pos+3] if m3 == 1 else inp[inp[pos+3]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            if m2 == 2: p2 = inp[inp[pos+2] + relb]
+            if m3 == 2: p3 = inp[inp[pos+3] + relb]
+            if p1 < p2:
+                inp[p3] = 1
             else:
-                set_inp(p3, "0")
+                inp[p3] = 0
             pos+= 4
         elif op == 8:#EQ
-            p1, p2, p3 = get_params(m1, m2, m3)
-            set_inp(p3, "1" if int(p1) == int(p2) else "0")
+            p1 = inp[pos+1] if m1 == 1 else inp[int(inp[pos+1])]
+            p2 = inp[pos+2] if m2 == 1 else inp[int(inp[pos+2])]
+            p3 = inp[pos+3] if m3 == 1 else inp[inp[pos+3]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            if m2 == 2: p2 = inp[inp[pos+2] + relb]
+            if m3 == 2: p3 = inp[inp[pos+3] + relb]
+            inp[p3] = 1 if p1 == p2 else 0
             pos+= 4
         elif op == 9:#SET RELB
-            p1, p2, p3 = get_params(m1, m2, m3)
-            # if m1 == 0:
-            #     relb+= int(get_inp(int(get_inp(pos+1))))
-            # elif m1 == 1:
-            #     relb+= int(get_inp(pos+1))
-            # elif m1 == 2:
-            #      relb+= int(get_inp(int(get_inp(pos+1))+ relb))
-            relb+= int(p1)
+            p1 = inp[pos+1] if m1 == 1 else inp[inp[pos+1]]
+            if m1 == 2: p1 = inp[inp[pos+1] + relb]
+            relb+= p1
             pos+= 2
         elif op == 99:
             print('Halt: %s' % output)
