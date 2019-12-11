64f4e2e883bc05e4bab292832a2ccf6620e70b19
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2019-12-11 01:04:02 -0500

    Day 10

--- AocPython/src/myAoc/2019/Day10.py
+++ AocPython/src/myAoc/2019/Day10.py
@@ -1,46 +1,55 @@
-from collections import defaultdict
 from fractions import Fraction
+from collections import defaultdict
+from math import atan2
+from math import pi
+import itertools
 
 rocks = []
 scores = {}
 
-def ratios_equal(r1, r2):
-    res1 = (r1[0] * r2[1], r1[1] * r2[1])
-    res2 = (r2[0] * r1[1], r2[1] * r1[1])
-    return res1[0] == res2[0] and res1[1] == res2[1]
+def rotate(d):
+    rot_d = {}
+    for k,v in d.items():
+        rot_d[atan2(k[0], k[1])] = v
+    while True:
+        start = False
+        for item in itertools.cycle(sorted(rot_d.items(), key=lambda x: x[0])):
+            if start or item[0] == pi/2:
+                start = True
+                yield item
 
-def get_mon_count(rock):
-    slopes = set()
+def kill_rocks(base, n):
+    slopes = scores[base]
+    d = rotate(slopes)
+    killed = 0
+    while killed < n:
+        l = next(d)
+        r = l[1].pop(0)
+        killed+= 1
+    return r[0]*100 + r[1]
+
+def get_mon_data(rock):
+    slopes = defaultdict(list)
     for _rock in [_rock for _rock in rocks if _rock != rock]:
         xdelt = rock[0] - _rock[0]
         ydelt = rock[1] - _rock[1]
         slope = (ydelt, xdelt)
-        # if xdelt != 0:
-        #     f = Fraction(ydelt, xdelt)
-        #     slope = (f.numerator, f.denominator)
-        # else:
-        #     slope = (float("inf") if ydelt > 0 else -float("inf"), 0)
-        l = len(slopes)
-        slopes.add(slope)
-        if l == len(slopes):
-            print('slope not added %s, %s' % (slope))
-        # found = False
-        # for s in slopes:
-        #     if ratios_equal(s, slope):
-        #         found = True
-        #         break
-        # if not found: slopes.add(slope)
-        # if xdelt == 0:
-        #     slope = (float("inf"), float("inf")) if ydelt > 0 else (-float("inf"), -float("inf"))
-        # else:
-        #     slope = (ydelt, xdelt)
-        # slopes.add(slope)
-   
-    # if len(slopes) == 214: print(slopes)
-    print(slopes)
-    return len(slopes)
+        #swine Fraction class messes up the signs!
+        signs = (-1 if slope[0] < 0 else 1, -1 if slope[1] < 0 else 1)
+        if xdelt != 0:
+            f = Fraction(ydelt, xdelt)
+            slope = (abs(f.numerator) * signs[0], abs(f.denominator) * signs[1])
+        else:
+            slope = (float("inf") if ydelt > 0 else -float("inf"), 0)
+        slopes[slope].append(_rock)
+    
+    for k, v in slopes.items():
+        v = sorted(v, key=lambda x: abs(rock[0] - x[0]) + abs(rock[1] - x[1]))
+        slopes[k] = v
 
-f = open('2019/data/day10a')
+    return slopes
+
+f = open('2019/data/day10')
 y = 0
 for line in f:
     x = 0
@@ -52,10 +61,11 @@ for line in f:
 total_rocks = len(rocks)
 
 for rock in rocks:
-    scores[rock] = get_mon_count(rock)
+    scores[rock] = get_mon_data(rock)
 
-best_rock = [(rock, count) for rock, count in scores.items() if count == max(scores.values())][0]
-print('Part 1: %s monitors %d asteroids' % (best_rock))
+best = sorted(scores.items(), key=lambda x: len(x[1]), reverse=True)[0]
+print('Part 1: %s monitors %d asteroids' % (best[0], len(best[1])))
+print('Part 2: %d' % kill_rocks(best[0], 200))
 
-# print(len(rocks))
-#Part 1: 160 < ans < 352 not: 259,264,275,276,283  between 275 and 283? 280, 281, 282
+#Part 1: 286
+#Part 2: 504
