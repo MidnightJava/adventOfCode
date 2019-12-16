060a2872eb3f8717694a2bc0c7057cd298136196
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2019-12-16 13:13:26 -0500

    Notional solution for Day 14

--- AocPython/src/myAoc/2019/Day14.py
+++ AocPython/src/myAoc/2019/Day14.py
@@ -6,7 +6,7 @@ pprint = _pprint.PrettyPrinter().pprint
 _map = {}
 ore_sources = set()
 
-f = open('2019/data/day14d')
+f = open('2019/data/day14a')
 for line in f:
     parts = line.split('=>')
     _prod = parts[1].split()
@@ -23,6 +23,7 @@ pprint(_map)
 print(ore_sources)
 
 ore_users_dict = defaultdict(int)
+ore_users_count_dict = defaultdict(int)
 def get_ore(ele, needed):
     global ore_users_dict
     ingredients = _map[ele][1]
@@ -31,16 +32,25 @@ def get_ore(ele, needed):
     for chem in ingredients:
         if chem[1] in ore_sources:
             ore_users_dict[chem[1]]+=(chem[0] * need)
+            ore_users_count_dict[chem[1]]+= 1
         else:
             get_ore(chem[1], chem[0] * need)
 
 get_ore('FUEL', 1)
 pprint(ore_users_dict.items())
+pprint(ore_users_count_dict.items())
 ore = 0
 for k,v in ore_users_dict.items():
     yld = _map[k][0]
     quantum = _map[k][1][0][0]
     ore_used = ceil(float(v) / float(yld)) * quantum
+    #Use quantized value of ore in setting values in ore_users_dict
+    #And calculate excess over what was actually needed
+    #net_ore = actual - excess
+    #ore_used = netx value up from net_ore that is a mult of _map[k][0]
+    #Ex a for item A: ore_used = 40; excess = 12; net_ore = 28
+    #so we actually used 30, since it comes in quantities of 10
+    #ore_used-= extra_ore
     ore+= ore_used
 
 print('Part 1: %d' % ore)
