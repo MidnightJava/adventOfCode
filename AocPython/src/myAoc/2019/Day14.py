f06b2f0f22f713e39951e3d993dc8e3a1986c140
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2019-12-17 00:56:43 -0500

    Day 14 attempts

--- AocPython/src/myAoc/2019/Day14.py
+++ AocPython/src/myAoc/2019/Day14.py
@@ -6,7 +6,7 @@ pprint = _pprint.PrettyPrinter().pprint
 _map = {}
 ore_sources = set()
 
-f = open('2019/data/day14a')
+f = open('2019/data/day14d')
 for line in f:
     parts = line.split('=>')
     _prod = parts[1].split()
@@ -22,35 +22,41 @@ for line in f:
 pprint(_map)
 print(ore_sources)
 
-ore_users_dict = defaultdict(int)
-ore_users_count_dict = defaultdict(int)
+ore_needed_dict = defaultdict(int)
+extra_ore_dict = defaultdict(int)
 def get_ore(ele, needed):
-    global ore_users_dict
+    global ore_needed_dict
     ingredients = _map[ele][1]
     yld = _map[ele][0]
     need = ceil(float(needed) / float(yld))
     for chem in ingredients:
         if chem[1] in ore_sources:
-            ore_users_dict[chem[1]]+=(chem[0] * need)
-            ore_users_count_dict[chem[1]]+= 1
+            ore_needed_dict[chem[1]]+=( chem[0] * need)
+            extra_ore_dict[chem[1]]+= (_map[chem[1]][0] * need)
         else:
             get_ore(chem[1], chem[0] * need)
 
 get_ore('FUEL', 1)
-pprint(ore_users_dict.items())
-pprint(ore_users_count_dict.items())
+pprint(ore_needed_dict.items())
+pprint(extra_ore_dict.items())
 ore = 0
-for k,v in ore_users_dict.items():
+for k,v in ore_needed_dict.items():
     yld = _map[k][0]
     quantum = _map[k][1][0][0]
-    ore_used = ceil(float(v) / float(yld)) * quantum
+    
     #Use quantized value of ore in setting values in ore_users_dict
     #And calculate excess over what was actually needed
     #net_ore = actual - excess
-    #ore_used = netx value up from net_ore that is a mult of _map[k][0]
+    #ore_used = next value up from net_ore that is a mult of _map[k][0]
     #Ex a for item A: ore_used = 40; excess = 12; net_ore = 28
     #so we actually used 30, since it comes in quantities of 10
     #ore_used-= extra_ore
+    if v % _map[k][0] != 0:
+        for i in range(v, v + _map[k][0]):
+            if i % _map[k][0] == 0:
+                v = i
+                break
+    ore_used = ceil(float(v) / float(yld)) * quantum
     ore+= ore_used
 
 print('Part 1: %d' % ore)
