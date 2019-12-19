7e8b4c80352c700de3e1a05898eda0afb8467076
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2019-12-18 22:09:27 -0500

    Day 14 mods

--- AocPython/src/myAoc/2019/Day14.py
+++ AocPython/src/myAoc/2019/Day14.py
@@ -4,9 +4,10 @@ from math import floor
 from collections import defaultdict
 pprint = _pprint.PrettyPrinter().pprint
 _map = {}
-ore_sources = set()
+extra = defaultdict(int)
+ore = 0
 
-f = open('2019/data/day14d')
+f = open('2019/data/day14a')
 for line in f:
     parts = line.split('=>')
     _prod = parts[1].split()
@@ -16,57 +17,34 @@ for line in f:
     for inp in inpl:
         _inp = inp.split()
         _inpl.append((int(_inp[0].strip()), _inp[1].strip()))
-        if _inp[1] == 'ORE': ore_sources.add(prod)
     _map[prod] = [int(_prod[0].strip()), _inpl]
 
 pprint(_map)
-print(ore_sources)
 
 ore_needed_dict = defaultdict(int)
 extra_ore_dict = defaultdict(int)
 def get_ore(ele, needed):
-    global ore_needed_dict
+    global ore
     ingredients = _map[ele][1]
     yld = _map[ele][0]
     need = ceil(float(needed) / float(yld))
+    if ele == 'ORE':
+        ore+= (need * _map[ele][0])
+        return
     for chem in ingredients:
-        if chem[1] in ore_sources:
-            ore_needed_dict[chem[1]]+=( _map[chem[1]][0] * need)
-            extra_ore_dict[chem[1]]+=(((_map[chem[1]][0] * need) % chem[0]) * need)
-        else:
-            get_ore(chem[1], chem[0] * need)
+        if chem[1] == 'ORE':
+            ore+= (chem[0] * need)
+            return
+        elif extra[chem[1]]:
+            used = min(need, extra[chem[1]])
+            need-= used
+            extra[chem[1]]-= used
+        extra[chem[1]]+= (chem[0] * need) % _map[chem[1]][0]
+        get_ore(chem[1], chem[0] * need)
 
 get_ore('FUEL', 1)
-pprint(ore_needed_dict.items())
-pprint(extra_ore_dict.items())
-ore = 0
-for k,v in ore_needed_dict.items():
-    yld = _map[k][0]
-    quantum = _map[k][1][0][0]
-    
-    #Use quantized value of ore in setting values in ore_users_dict
-    #And calculate excess over what was actually needed
-    #net_ore = actual - excess
-    #ore_used = next value up from net_ore that is a mult of _map[k][0]
-    #Ex a for item A: ore_used = 40; excess = 12; net_ore = 28
-    #so we actually used 30, since it comes in quantities of 10
-    #ore_used-= extra_ore
-    # if v % _map[k][0] != 0:
-    #     for i in range(v, v + _map[k][0]):
-    #         if i % _map[k][0] == 0:
-    #             v = i
-    #             break
-    # chem_used = v + extra_ore_dict[k]
-    # if v % _map[k][0] != 0:
-    #     for i in range(v, v +  _map[k][0]):
-    #          if i % _map[k][0] == 0:
-    #              v = i
-    #              break
-    net_chem = v - extra_ore_dict[k]
-    ore_used = ceil(float(net_chem) / float(yld)) * quantum
-    ore+= ore_used
-
 print('Part 1: %d' % ore)
+pprint(extra)
 
 # Part 1: 637,682 too low, 10,645,906 too high not 1,693,718
 
