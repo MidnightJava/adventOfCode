f0e1db7af66025e7579e1c41137593442a441018
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2020-01-07 18:44:34 -0500

    Day 16 part 2 attempt

--- AocPython/src/myAoc/2019/Day16.py
+++ AocPython/src/myAoc/2019/Day16.py
@@ -1,25 +1,45 @@
 from itertools import cycle
 
+# inp = open('2019/data/day16').read()
+# for n in range(100):
+#     outp  = ''
+#     for j in range(1, len(inp)+1):
+#         tot = 0
+#         pattern = cycle([0,1,0,-1])
+#         p = next(pattern)
+#         for i in range(1, len(inp)+1):
+#             if i % j == 0: p = next(pattern)
+#             v = int(inp[i-1])
+#             tot+= (v * p)
+#         outp+= str(tot)[-1]
+#     inp = outp
+
+# print('Part 1: %s' % outp[:8])
+
 inp = open('2019/data/day16').read()
-inp*= 1000
+inp = "03036732577212944063491565474664"
+inp*= 10000
 offset = int(inp[:7])
-print('Offset', offset)
+print('Offset', inp[:7], offset)
 print('Len input', len(inp))
-# inp ="12345678"
-# inp ="80871224585914546619083218645595"
 
 for n in range(100):
+    print("*" + inp[:8] + "*")
     outp  = ''
-    for j in range(1, len(inp)+1):
-        tot = 0
-        pattern = cycle([0,1,0,-1])
-        p = next(pattern)
-        for i in range(1, len(inp)+1):
-            if i % j == 0: p = next(pattern)
-            v = int(inp[i-1])
-            tot+= (v * p)
+    tot = sum(map(int, list(inp[offset:])))
+    for j in range(offset, len(inp)):
+        # pattern = cycle([0,1,0,-1])
+        # p = next(pattern)
+        for i in range(j, len(inp)):
+            # if i % j == 0: p = next(pattern)
+            tot-= int(inp[i])
         outp+= str(tot)[-1]
     inp = outp
 
-# print('Part 1: %s' % outp[:8])
-print('Part 2: %s' % outp[offset:])
+print('Part 2: %s' % outp)
+
+"""
+Test Data:
+Offset 303673
+Len input 320000
+"""
