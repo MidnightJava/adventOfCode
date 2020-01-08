5466b8fbb2789302ebfce1831e36b3dda87f8fd6
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2020-01-08 00:06:55 -0500

    Day 16 part 2 attempts

--- AocPython/src/myAoc/2019/Day16.py
+++ AocPython/src/myAoc/2019/Day16.py
@@ -16,27 +16,47 @@ from itertools import cycle
 
 # print('Part 1: %s' % outp[:8])
 
-inp = open('2019/data/day16').read()
+# inp = open('2019/data/day16').read()
 inp = "03036732577212944063491565474664"
 inp*= 10000
+answ = '84462026'
 offset = int(inp[:7])
-print('Offset', inp[:7], offset)
-print('Len input', len(inp))
+print('Offset', offset)
+
 
 for n in range(100):
-    print("*" + inp[:8] + "*")
-    outp  = ''
     tot = sum(map(int, list(inp[offset:])))
-    for j in range(offset, len(inp)):
-        # pattern = cycle([0,1,0,-1])
-        # p = next(pattern)
-        for i in range(j, len(inp)):
-            # if i % j == 0: p = next(pattern)
-            tot-= int(inp[i])
-        outp+= str(tot)[-1]
-    inp = outp
+    # print(inp[offset:offset + 8])
+    outp  = ''
+    subtr = 0
+    for j in range(offset+3, len(inp)):
+        subtr+= int(inp[j])
+        outp+= str(tot - subtr)[-1]
+    inp = outp.rjust(len(inp), '0')
+    if answ in outp: print('Found',  n, outp.index(answ))
 
-print('Part 2: %s' % outp)
+print('Part 2: %s' % (inp[offset:offset+8]))
+
+inp = '02935109699940807407585447034323'
+inp*= 10000
+answ = '78725270'
+offset = int(inp[:7])
+print('Offset', offset)
+
+
+for n in range(100):
+    tot = sum(map(int, list(inp[offset:])))
+    # print(inp[offset:offset + 8])
+    outp  = ''
+    subtr = 0
+    # print('offset', inp[offset:offset+7])
+    for j in range(offset, len(inp)):
+        subtr+= int(inp[j])
+        outp+= str(tot - subtr)[-1]
+    inp = outp.rjust(len(inp), '0')
+    if answ in outp: print('Found',  n, outp.index(answ))
+
+# print('Part 2: %s' % res)
 
 """
 Test Data:
