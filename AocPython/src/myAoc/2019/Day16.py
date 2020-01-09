6d008a73f205b529bc96c61d17570482f10b1b97
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2020-01-08 23:15:39 -0500

    Day 16 part 2

--- AocPython/src/myAoc/2019/Day16.py
+++ AocPython/src/myAoc/2019/Day16.py
@@ -1,74 +1,35 @@
 from itertools import cycle
 
-# inp = open('2019/data/day16').read()
-# for n in range(100):
-#     outp  = ''
-#     for j in range(1, len(inp)+1):
-#         tot = 0
-#         pattern = cycle([0,1,0,-1])
-#         p = next(pattern)
-#         for i in range(1, len(inp)+1):
-#             if i % j == 0: p = next(pattern)
-#             v = int(inp[i-1])
-#             tot+= (v * p)
-#         outp+= str(tot)[-1]
-#     inp = outp
+inp = open('2019/data/day16').read()
+for n in range(100):
+    outp  = ''
+    for j in range(1, len(inp)+1):
+        tot = 0
+        pattern = cycle([0,1,0,-1])
+        p = next(pattern)
+        for i in range(1, len(inp)+1):
+            if i % j == 0: p = next(pattern)
+            tot+= (int(inp[i-1]) * p)
+        outp+= str(tot)[-1]
+    inp = outp
 
-# print('Part 1: %s' % outp[:8])
+print('Part 1: %s' % outp[:8])
 
-# inp = open('2019/data/day16').read()
-# inp = "03036732577212944063491565474664"
-# offset = int(inp[:7])
-# print('Offset', offset)
-# inp*= 10000
-# inp = inp[offset:]
-# answ = '84462026'
+inp = open('2019/data/day16').read()
+inp*= 10000
+offset = int(inp[:7])
+inp = inp[offset:]
 
+for n in range(100):
+    tot = sum(map(int, list(inp)))
+    outp  = ''
+    subtr = 0
+    for j in range(len(inp)):
+        outp+= str(tot - subtr)[-1]
+        subtr+= int(inp[j])
+    inp = outp
 
+print('Part 2: %s' % inp[:8])
 
-# for n in range(100):
-#     tot = sum(map(int, list(inp)))
-#     # print(inp[:8])
-#     outp  = ''
-#     subtr = 0
-#     for j in range(len(inp)):
-#         subtr+= int(inp[j])
-#         outp+= str(tot - subtr)[-1]
-#     inp = outp
-#     if answ in outp: print('Found',  n, outp.index(answ))
-
-# print('Part 2: %s' % (inp[:8]))
-
-inps = ["03036732577212944063491565474664", "02935109699940807407585447034323", "03081770884921959731165446850517"]
-answs = ['84462026', '78725270', '53553731']
-corrections = [7868, 7900, 3932]
-
-for test in range(3):
-    inp = inps[test]
-    answ = answs[test]
-    inp*= 10000
-    offset = int(inp[:7])
-    print('Offset', offset)
-    offset+= + corrections[test]
-    print('sum', sum(map(int, list(inp[:7868]))))
-    inp = inp[offset:]
-
-    for n in range(100):
-        # print('len', len(inp))
-        tot = sum(map(int, list(inp)))
-        # print(inp[:8])
-        outp  = ''
-        subtr = 0
-        for j in range(len(inp)):
-            subtr+= int(inp[j])
-            outp+= str(tot - subtr)[-1]
-        inp = outp
-        if answ in inp: print('Found after %d iterations at offset %d' % (n+1, inp.index(answ)))
-
-    print('Part 2: %s' % inp[:8])
-
-"""
-Test Data:
-Offset 303673
-Len input 320000
-"""
+#Part 1: 27831665
+#Part 2: 36265589
