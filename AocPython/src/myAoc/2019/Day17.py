7d64e2e4069e9853a1e2c5e0b384442194c80e6a
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2020-01-12 22:33:07 -0500

    Day 17 part 2

--- AocPython/src/myAoc/2019/Day17.py
+++ AocPython/src/myAoc/2019/Day17.py
@@ -1,3 +1,8 @@
+from itertools import permutations
+import re
+intersections = set()
+loc = None
+
 class Proc:
     def __init__(self, code):
         self.code = code
@@ -77,9 +82,8 @@ def intersection(grid, x, y, max_x, max_y):
             else: res&= (grid[(x,y-1)] == '#' and grid[(x,y+1)] == '#')
         else:
             res = False
+        if res: intersections.add((x,y))
         return res
-
-
 
 def run(p, rules=None, part1=False):
     grid = {}
@@ -102,6 +106,7 @@ def run(p, rules=None, part1=False):
             x+= 1
         elif res == 94:
             grid[(x,y)] = '^'
+            loc = (x,y)
             x+= 1
         elif res == 118:
             grid[(x,y)] = 'v'
@@ -114,8 +119,11 @@ def run(p, rules=None, part1=False):
             x+= 1
         else:
             grid[(x,y)] = chr(res)
-            if res > 255: print('CHAR:', res)
-            print(chr(res), end='')
+            if res <= 255:
+                print(chr(res), end='')
+            else:
+                print()
+                print('Part 2: %d' % res)
         
 
     max_y = max(map(lambda x: x[1], grid.keys()))
@@ -129,14 +137,119 @@ def run(p, rules=None, part1=False):
             y+= 1
 
         print('Part 1: %d' % score)
-    print_grid(grid, max_x, max_y)
-    print()
+    # print_grid(grid, max_x, max_y)
+    # print()
+
+    moves = ['L']
+    direc = 3 #o: up, 1: right, 2: down, 3: left
+    grid[loc] = '#'
+    x, y = loc
+    while True:
+        dist = 0
+        while (x,y) in grid and grid[(x,y)] == '#':
+            last_loc = x, y
+            if direc == 0: y-= 1
+            elif direc == 1: x+= 1
+            elif direc == 2: y+= 1
+            elif direc == 3: x-= 1
+            dist+= 1
+        if dist == 1: break
+        x,y = last_loc
+        moves.append(dist-1)
+        if direc == 1 or direc == 3:
+            # go up
+            if grid.get((x,y-1)) == '#':
+                if direc == 3: moves.append('R')
+                elif direc == 1: moves.append('L')
+                direc = 0
+            # go down
+            elif grid.get((x,y+1)) == '#':
+                if direc == 1: moves.append('R')
+                elif direc == 3: moves.append('L')
+                direc = 2
+        elif direc == 0 or direc == 2:
+            # go right
+            if grid.get((x+1,y)) == '#':
+                if direc == 0: moves.append('R')
+                elif direc == 2: moves.append('L')
+                direc = 1
+            # go left
+            elif  grid.get((x-1,y)) == '#':
+                if direc == 2: moves.append('R')
+                elif direc == 0: moves.append('L')
+                direc = 3
+    if part1: return getRules("".join(map(str, moves)))
+
+def substrings(s, n):    
+    st = set()  
+    for i in range(len(s)):  
+        ans = ""
+        for j in range(i, len(s)): 
+            ans += s[j]    
+            if (len(ans) == n):
+                st.add(ans)
+                break
+
+    return list(st)
+    
+def getRules(moves):
+    all_ss = []
+    for n in range(3,12):
+        all_ss+= substrings(moves, n)
+    for p in permutations(all_ss, 3):
+        res = fit(moves, p[0], p[1], p[2])
+        if res:
+            return res[::-1]
+
+def insert_commas(l):
+    l2 = []
+    i = 0
+    while i < len(l)-1:
+        if re.match(r"\d{2}", l[i] + l[i+1]):
+            l2+= [l[i], l[i+1], ',']
+            i+= 2
+        else:
+            l2+= [l[i], ',']
+            i+= 1
+    if not re.match(r"\d{2}", l[-1] + l[-2]):
+        l2.append(l[-1])
+    else:
+         l2 = l2[:-1]
+    return l2
+
+def fit(moves, a, b,c):
+    i = 0
+    while i < len(moves):
+        if moves[i: i+len(a)] == a:
+            i+= len(a)
+        elif moves[i: i+len(b)] == b:
+            i+= len(b)
+        elif moves[i: i+len(c)] == c:
+            i+= len(c)
+        else:
+            return False
+
+    m_list = []
+    for i in range(len(moves)):
+        if moves[i: i+len(c)] == c: m_list.append('C')
+        if moves[i: i+len(b)] == b: m_list.append('B')
+        if moves[i: i+len(a)] == a: m_list.append('A')
+    m_list = insert_commas(m_list)
+    m_list.append('\n')
+    m_list+= insert_commas(list(a))
+    m_list.append('\n')
+    m_list+= insert_commas(list(b))
+    m_list.append('\n')
+    m_list+= insert_commas(list(c))
+    m_list.append('\n')
+    m_list+= ['n', '\n']
+    return list(map(ord, m_list))
 
 f = open('./2019/data/day17')
 code = list(map(int, f.readline().split(',')))
 [code.append(0) for _ in range(10000)]
 p = Proc(code)
-run(p, part1=True)
+rules = run(p, part1=True)
 
 
 f = open('./2019/data/day17')
@@ -144,7 +257,7 @@ code = list(map(int, f.readline().split(',')))
 [code.append(0) for _ in range(10000)]
 code[0] = 2
 p = Proc(code)
-rules = [65,44,66,44,67,10,76,10,49,44,49,44,49,10,49,44,49,44,49,10,121,10][::-1]
-run(p, rules)
-#Part 1: 6672
+run(p, rules, part1=False)
 
+#Part 1: 6672
+#Part 2: 923017
