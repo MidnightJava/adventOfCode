79f3b9298a551381b89700e1e954d4b0892d6c4e
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2020-01-14 11:36:14 -0500

    Use tuples in grid instead of array of arrays

--- AocPython/src/myAoc/2019/Day18.py
+++ AocPython/src/myAoc/2019/Day18.py
@@ -2,47 +2,54 @@ import re
 import heapq
 from collections import deque
 
-grid = []
+grid = {}
 doors = dict()
 keys = set()
 
 f = open('2019/data/day18')
 y = 0
 for line in f:
-    row = []
     x = 0
     for c in line.strip():
         if re.match(r"[A-Z]", c):
-            doors[c] = (y, x)
+            doors[c] = (x, y)
         elif re.match(r"[a-z]", c):
             keys.add(c)
-        elif c== '@': entr = (y,x)
-        row.append(c)
+        elif c== '@': entr = (x,y)
+        grid[(x,y)] = c
         x+= 1
-    grid.append(row)
+    x_max = x
     y+= 1
+y_max = y
 
-for row in grid:
-    print("".join(row))
+def print_grid():
+    for y in range(y_max):
+        for x in range(x_max):
+            print(grid[(x,y)], end='')
+        print()
+
+print_grid()
+print(doors)
+print(keys)
 
 seen = set()
-y,x = entr
-grid[y][x] = '.'
-queue = deque([(y,x)])
+x,y = entr
+grid[(x,y)] = '.'
+queue = deque([(x,y)])
 d = 0
 while len(queue)>0:
-    y,x = queue.popleft()
-    if (y,x) in seen:
+    x,y = queue.popleft()
+    if (x,y) in seen:
         continue
-    seen.add((y,x))
-    neighbors = [n for n in [(y, x-1), (y, x+1), (y-1, x), (y+1,x)] if grid[n[0]][n[1]] == '.' or re.match(r"[a-z]", grid[n[0]][n[1]])]
+    seen.add((x,y))
+    neighbors = [n for n in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)] if grid[n] == '.' or re.match(r"[a-z]", grid[n])]
     d+= 1
     for nb in neighbors:
-        c = grid[nb[0]][nb[1]]
+        c = grid[nb]
         if re.match(r"[a-z]", c):
-            _y,_x = doors[c.upper()]
-            grid[_y][_x] = '.'
-            grid[nb[0]][nb[1]] = '.'
+            _x, _y = doors[c.upper()]
+            grid[(_x, _y)] = '.'
+            grid[nb] = '.'
             seen = set()
             keys.remove(c)
             if len(keys) == 0: print('min path', d)
@@ -51,4 +58,4 @@ while len(queue)>0:
 
 print('min path', d)
 
-#Part 1: 2453 too low not 67942, 22605, 19404
+#Part 1: 2453 too low not 67942, 22605, 19404, 32500, 35701
