f653ac1cf8a912faf1a0f813cb86eaa625b364b9
Leone, Mark A [Axios] <mark.leone@caci.com>
2021-12-27 16:42:45 -0500

    Tweaks

--- AocPython/src/myAoc/2021/Day14.py
+++ AocPython/src/myAoc/2021/Day14.py
@@ -3,47 +3,33 @@ from collections import defaultdict
 d = {}
 lines = open('2021/data/day14').readlines()
 
-temp = lines[0].strip()
+temp1 = lines[0].strip()
 for i in range(2, len(lines)):
-    parts = lines[i].split('->')
-    d[parts[0].strip()] = parts[1].strip()
+    vals = lines[i].split('->')
+    d[vals[0].strip()] = vals[1].strip()
 
-# Brute force method. Works only for part 1
-# for step in range(40):
-#     subs = []
-#     for i in range(0,len(temp)-1):
-#         subs.append(d[temp[i] + temp[i+1]])
-#     subs = subs[::-1]
-#     templ = list(temp)
-#     i = len(templ) - 1
-#     for sub in subs:
-#         templ.insert(i, sub)
-#         i-= 1
-#     temp = ''.join(templ)
+f1 = defaultdict(int) # freq table for single characters
+f2 = defaultdict(int) # freq table for contiguous 2-character values
+for c in temp1: f1[c]+= 1
+for i in range(len(temp1)-1): f2[temp1[i:i+2]]+= 1
 
-# f = defaultdict(int)
-# for c in temp: f[c]+= 1
-# res = max(f.values()) - min(f.values())
-# print('Part 1: %d' % res)
-
-f1 = defaultdict(int)
-f2 = defaultdict(int)
-for c in temp: f1[c]+= 1
-for i in range(len(temp)-1):
-    f2[temp[i:i+2]]+= 1
 for step in range(40):
     mods = []
+    # Remove count of existing 2-char keys and add entries for two keys with
+    # inserted product character, with same count.
     for key in f2.keys():
         sub = d[key]
-        f1[sub]+= f2[key]
+        n = f2[key]
+        f1[sub]+= n
         k2 = key[0] + sub
         k3 = sub + key[1]
-        mods.append((k2, f2[key]))
-        mods.append((k3, f2[key]))
-        mods.append((key, -f2[key]))
-    for mod in mods:
-        k,val = mod
-        f2[k]+= val
+        # Track dict mods to apply after iterating
+        mods.append((k2, n))
+        mods.append((k3, n))
+        mods.append((key, -n))
+
+    for k,val in mods: f2[k]+= val
+
     if step == 9:
         res = max(f1.values()) - min(f1.values())
         print('Part 1: %d' % res)
