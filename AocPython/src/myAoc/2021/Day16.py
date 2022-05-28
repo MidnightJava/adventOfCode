d583a042b0f1b7b13f92b114d1feabb8cf30a40d
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2022-05-27 22:15:49 -0400

    Part 1 working

--- AocPython/src/myAoc/2021/Day16.py
+++ AocPython/src/myAoc/2021/Day16.py
@@ -1,9 +1,9 @@
 
 vsum = 0
 
-def process(bits, pkts):
+def process(bits):
+    if len(bits) <= 11: return
     global vsum
-    # while len(bits) and pkts >= 1:
     vbits = bits[:3]
     tbits = bits[3:6]
     ver= int(vbits, 2)
@@ -15,16 +15,17 @@ def process(bits, pkts):
         done = False
         sum = ''
         while not done:
-            gbits= bits[:5]
+            gbits = bits[:5]
             bits = bits[5:]
             sum+= gbits[1:]
             if gbits[0] == '0':
                 done = True
-                bits = bits[5:]
+                if len(bits):
+                    process(bits[::])
                 # while len(bits) % 4 != 0:
                 #     bits = bits[:-1]
-                # print(f"SUM {int(sum,2)}")
-        # pkts-= 1
+                
+        return int(sum,2)
     else:
         ibit = bits[0]
         bits = bits[1:]
@@ -32,31 +33,27 @@ def process(bits, pkts):
             lbits = bits[:15]
             bits = bits[15:]
             l = int(lbits, 2)
-            process(bits[::], 1)
-            pkts-= 1
+            process(bits[::])
         elif ibit == '1':
             lbits = bits[:11]
             bits = bits[11:]
             l = int(lbits, 2)
-            process(bits[::], l)
-            pkts-= 1
+            process(bits[::])
         else:
             print("Invalid packet length type ID")
 
-    while len(bits) % 8 != 0:
-        bits = bits[:-1]
+    # while len(bits) % 8 != 0:
+    #     bits = bits[:-1]
 
-    # print(bits)
-
-for line in open('2021/data/day16b').readlines():
+for line in open('2021/data/day16').readlines():
     vsum = 0
     msg = line.strip()
     bits =  bin(int(msg,16))[2:] #strip off leading '0b'
     while len(bits) % 4 != 0:
         bits = '0' + bits
     print(f"Initial bits: {bits}")
-    process(bits[::], 1)
+    process(bits[::])
 
     print('Part 1: %d' % vsum)
 
-
+# Part 1: 979
