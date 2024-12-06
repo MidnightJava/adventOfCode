c34f21a60a0dce61039cfd2959d88f67d3aa2f7d
Leone, Mark A [LGS] <mleone607@gmail.com>
2024-12-05 20:38:32 -0500

    Day 5 part 2

--- AocPython/src/myAoc/2024/Day05.py
+++ AocPython/src/myAoc/2024/Day05.py
@@ -1,22 +1,49 @@
+""" 
+Input is in two parts:
+
+records like: x|Y represent a rule specifying that page y cannot be added to a list until page x is already in the list
+
+records like x,y,z... represent a list of pages in a manual update
+
+Part 1:
+
+Determine which update lists are valid wrt any rules specified for the pages in the list. For each valid update, determine
+the page in the middle of the update list. Add up all the middle page numbers and report that as the solution.
+
+Part 2:
+
+Collect all the updates that are not valid per instructions in Part 1. For each uf these, correct the order of the pages so
+the update follows all the rules.  determine the middle page of each update list, add them up, and report that as the solution.
+"""
+
 from collections import defaultdict
 
 def update_valid(update, rules):
     valid = True
-    for page in rules.keys():
-        if page in update:
-            priors = rules[page]
-            for prior in priors:
-                if prior in update and update.index(prior) > update.index(page):
-                    valid = False
-                    break
+    for page in update:
+      for prior in rules[page]:
+        if prior in update and update.index(prior) > update.index(page):
+          valid = False
+          break
     return valid
 
-def correct_update(update, rules):
-    """
-    - Add all pages with no priors
-    - Recursively add pages whose priors already exist in the list
-    - Verify that all the pages got added.
-    """
+def correct_update(input, rules):
+  """
+  - Iterate over pages in the input list
+    - For any page with no rule assigned, move it from the input the output list
+  - Iterate through the input list until it's empty
+    - Move to the output list each pages whose priors already exist in the output list
+  """
+  output = []
+  for page in input:
+    if not page in rules:
+      output.append(input.pop(input.index(page)))
+  while len(input):
+    for page in input:
+      priors = filter(lambda x: x not in output, rules[page])
+      if all(map(lambda x: x in output or x not in input, priors)):
+        output.append(input.pop(input.index(page)) if page  in input else page)
+  return output
 
 def middle_page(update):
     idx = len(update) // 2
@@ -36,11 +63,8 @@ with open("2024/data/day05") as f:
             rule = line.split("|")
             rules[int(rule[1])].append(int(rule[0]))
         else:
-            update = line.strip().split(",")
+            update = line.split(",")
             updates.append(list(map(lambda x: int(x), update)))
-# print(rules)
-# for update in updates:
-#     print(update)
 
 count = 0
 for update in updates:
@@ -51,10 +75,11 @@ for update in updates:
 
 print(f"Part 1: {count}")
 
-count += 0
+count = 0
 for update in bad_updates:
-    correct_order(update, rules)
-    count +=  middle_page(update)
+    corrected = correct_update(update, rules)
+    count +=  middle_page(corrected)
 
 print(f"Part 2: {count}")
 # Part 1: 6951
+# Part 2: 4121
