7baef72e10bd7001f3e1f317832145e769c13782
Mark Leone <mleone@spectric.com>
2024-12-12 19:33:30 -0500

    Day 9 part 2 attempt

--- AocPython/src/myAoc/2024/Day09.py
+++ AocPython/src/myAoc/2024/Day09.py
@@ -1,5 +1,6 @@
 import re
 import time
+from collections import Counter
 
 def write_disk(s):
   disk = ""
@@ -39,28 +40,69 @@ def defrag(disk):
     # print(disk.index('.'))
   return disk
 
+# def calculate_score(disk):
+#   disk = disk.replace("|", "")
+#   score = 0
+#   idx = 0
+#   while True:
+#     m = re.search(r"\|(\d)\|", disk)
+#     if not m: break
+#     score += idx * int(m.group(1))
+#     disk = disk[m.end():]
+#     idx += 1
+#   return score
+
 def calculate_score(disk):
+  disk = disk.replace("|", "")
   score = 0
   idx = 0
-  while True:
-    m = re.search(r"\|(\d+)\|", disk)
-    if not m: break
-    score += idx * int(m.group(1))
-    disk = disk[m.end():]
+  for c in disk:
+    if c != ".":
+      score += idx * int(c)
     idx += 1
   return score
+
+def get_file_numbers(disk):
+  m = re.search(r"\|(\d+)\|$", disk)
+  initial_num = int(m.group(1))
+  for i in range(initial_num, -1, -1):
+    yield i
+
     
   
+# with open("2024/data/day09a") as f:
+#   map = f.read()
+#   start = time.time()
+#   # print(f"disk map: {map}")
+#   disk = write_disk(map)
+#   # print(f"disk: {disk}")
+#   disk  = defrag(disk)
+#   # print(disk)
+#   # print(disk.replace("|", ""))
+#   score = calculate_score(disk)
+#   print(f"Part 1: {score}, time: {time.time() - start:.0f} secs")
+
+def move_file(n, disk):
+  m1 = re.search(f"((?:\|{n}\|)+)", disk)
+  num = m1.group(1).count(f"|{n}|")
+  quant = "{" + f"{num}" + "}"
+  m2 = re.search(f"\.{quant}", disk[:m1.start()])
+  if m2:
+    blocks = m1.group(1)
+    disk = disk[:m1.start()]   + '.' * num + disk[m1.end():]
+    disk = disk[:m2.start()] + blocks + disk[m2.end():]
+  return disk
+
 with open("2024/data/day09") as f:
   map = f.read()
   start = time.time()
   # print(f"disk map: {map}")
   disk = write_disk(map)
   # print(f"disk: {disk}")
-  disk  = defrag(disk)
-  # print(disk)
-  # print(disk.replace("|", ""))
+  for i in get_file_numbers(disk):
+    disk = move_file(i, disk)
   score = calculate_score(disk)
-  print(f"Part 1: {score}, time: {time.time() - start:.0f} secs")
-  
+  print(f"Part 2: {score}, time: {time.time() - start:.0f} secs")
   # Part 1: 6337367222422
+
+  '|0||0||9||9||2||1||1||1||7||7||7|.|4||4|.|3||3||3|....|5||5||5||5|.|6||6||6||6|.....|8||8||8||8|..'
